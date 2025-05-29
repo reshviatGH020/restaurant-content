8c837abf21814f22378dfb096610ced90d541a84
reshviatGH020 <reshvi.wbds@gmail.com>
2025-05-29 07:11:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
